b950d18531e254996e8a894c546d8fb2861f5dda
Luis Zambrano <154038166+LuisAntonio06@users.noreply.github.com>
2025-03-09 22:33:24 -0600

    Add files via upload

--- js/appIndexNav.js
+++ js/appIndexNav.js
@@ -35,3 +35,18 @@ x    }
   })
   
   
+
+  
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    const offset = 80; // Espacio extra arriba
+    const position = target.offsetTop - offset;
+
+    window.scrollTo({
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
--- js/appProductoHref.js
+++ js/appProductoHref.js
@@ -1,24 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Selecciona todos los botones con la clase 'btn-ver-mas'
-    document.querySelectorAll(".btn-ver-mas").forEach(function (button) {
+    document.querySelectorAll(".principal__boton").forEach(function (button) {
         button.addEventListener("click", function () {
-            window.location.href = "producto.html";
+            window.location.href = "productos.html";
         });
     });
 });
 
 
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute("href"));
-    const offset = 80; // Espacio extra arriba
-    const position = target.offsetTop - offset;
-
-    window.scrollTo({
-      top: position,
-      behavior: "smooth"
-    });
-  });
-});
